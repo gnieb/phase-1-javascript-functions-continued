174eacdd55bdb57b6787b0a24b0acd46aa9dace2
Grace Nieboer <grace.nieboer@outlook.com>
2023-01-31 18:39:55 -0500

    YAY

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(string= "go to the office") {
+return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(string = "*") {
+    return function (param = "special"){
+        return `You are ${string}${param}${string}!`
+    }
+}
+
